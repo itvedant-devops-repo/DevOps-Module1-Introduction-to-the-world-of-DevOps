4b34d7163fca83c118cc14048970c78a32bf1039
itvedant-devops-repo <gopal.sharma@itvedant.com>
2025-01-31 17:28:42 +0530

    Update backup_script.sh

--- Scripts/backup_script.sh
+++ Scripts/backup_script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-# Script to back up a directory
+# TODO: Modify the script to back up a directory and add logging
 
 SOURCE_DIR="$HOME/Documents"
 BACKUP_DIR="$HOME/backup"
+
+# TODO: Add checks to ensure the directories exist before proceeding
 
 mkdir -p "$BACKUP_DIR"
 cp -r "$SOURCE_DIR" "$BACKUP_DIR"
